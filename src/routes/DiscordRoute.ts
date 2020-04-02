14b3c6a0b8ff27c1e37a754b7235dba71a341701
Amish Shah <amishshah.2k@gmail.com>
2020-04-02 22:50:27 +0100

    feat: Discord OAuth2 integration

---
+++ src/routes/DiscordRoute.ts
@@ -0,0 +1,26 @@
+import RouteHandler from '../RouteHandler';
+import HackathonAPI from '../HackathonAPI';
+import { Request, Response } from 'express';
+
+interface QueryParams {
+	code?: string;
+	state?: string;
+}
+
+export class DiscordRoute implements RouteHandler {
+	private readonly api: HackathonAPI;
+	public constructor(api: HackathonAPI) {
+		this.api = api;
+	}
+
+	public getRoute() {
+		return '/discord';
+	}
+
+	public async get(req: Request, res: Response) {
+		const { code, state } = req.query as QueryParams;
+		if (!code || !state) return res.status(400).json({ message: 'Missing query parameters' });
+		await this.api.controllers.discord.processOAuth2(code, state);
+		res.json({ message: 'ok' });
+	}
+}
