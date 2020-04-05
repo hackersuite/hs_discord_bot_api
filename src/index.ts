27ee3c4e1135841ab9126f663396197fd4596b5d
Amish Shah <amishshah.2k@gmail.com>
2020-04-05 16:01:09 +0100

    chore: initial docker files

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,48 @@
 import HackathonAPI, { HackathonAPIOptions } from './HackathonAPI';
 import pino from 'pino';
-const config = require('../data/config.json') as HackathonAPIOptions;
+
+function getEnv(name: string) {
+	const val = process.env[name];
+	if (typeof val === 'undefined') {
+		throw new Error(`Environment variable '${name}' is unset`);
+	}
+	return val;
+}
 
 const baseLogger = pino();
-config.loggers = {
-	base: baseLogger,
-	api: baseLogger.child({ name: 'api' })
+
+const config: HackathonAPIOptions = {
+	db: {
+		host: getEnv('DB_HOST'),
+		port: Number(getEnv('DB_PORT')),
+		username: getEnv('DB_USERNAME'),
+		password: getEnv('DB_PASSWORD'),
+		database: getEnv('DB_DATABASE'),
+		type: 'mysql'
+	},
+	api: {
+		port: Number(getEnv('PORT'))
+	},
+	discord: {
+		clientId: getEnv('DISCORD_CLIENT_ID'),
+		clientSecret: getEnv('DISCORD_CLIENT_SECRET'),
+		botToken: getEnv('DISCORD_BOT_TOKEN'),
+		guildId: getEnv('DISCORD_GUILD_ID'),
+		redirectUri: getEnv('DISCORD_REDIRECT_URI'),
+		hmacKey: getEnv('HMAC_KEY')
+	},
+	hsAuth: {
+		token: getEnv('HS_AUTH_TOKEN'),
+		url: getEnv('HS_AUTH_URL')
+	},
+	loggers: {
+		base: baseLogger,
+		api: baseLogger.child({ name: 'api' })
+	}
 };
 
-process.env.AUTH_URL = config.hsAuth.url;
+// For hs_auth_client
+process.env.AUTH_URL = getEnv('HS_AUTH_URL');
 
 const apiServer = new HackathonAPI(config);
 apiServer.start()
