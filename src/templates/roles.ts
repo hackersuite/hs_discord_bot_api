b4560c3a4ad2992cdc589c3f259daa7d72ad0d92
Amish Shah <amishshah.2k@gmail.com>
2020-04-22 17:05:41 +0100

    fix: permissions of roles

--- src/templates/roles.ts
+++ src/templates/roles.ts
@@ -1,33 +1,48 @@
-import { CreateGuildRoleData } from '../utils/DiscordConstants';
+import { CreateGuildRoleData, PermissionFlag } from '../utils/DiscordConstants';
+
+const volunteerPermissions =
+	PermissionFlag.MANAGE_MESSAGES |
+	PermissionFlag.MANAGE_NICKNAMES |
+	PermissionFlag.VIEW_AUDIT_LOG;
+
+const organiserPermissions = volunteerPermissions |
+	PermissionFlag.KICK_MEMBERS |
+	PermissionFlag.BAN_MEMBERS;
 
 export function organiser(): CreateGuildRoleData {
 	return {
 		name: 'Organiser',
-		hoist: true
+		hoist: true,
+		permissions: organiserPermissions
 	};
 }
 
 export function volunteer(): CreateGuildRoleData {
 	return {
-		name: 'Volunteer'
+		name: 'Volunteer',
+		hoist: true,
+		permissions: volunteerPermissions
 	};
 }
 
 export function attendee(): CreateGuildRoleData {
 	return {
-		name: 'Attendee'
+		name: 'Attendee',
+		permissions: 0
 	};
 }
 
 export function muted(): CreateGuildRoleData {
 	return {
-		name: `Muted`
+		name: `Muted`,
+		permissions: 0
 	};
 }
 
 export function team(teamNumber: number): CreateGuildRoleData {
 	return {
-		name: `Team ${teamNumber}`
+		name: `Team ${teamNumber}`,
+		permissions: 0
 	};
 }
 
@@ -35,6 +50,7 @@ export function language(name: string): CreateGuildRoleData {
 	return {
 		name,
 		mentionable: true,
-		color: 0x1ABC9C
+		color: 0x1ABC9C,
+		permissions: 0
 	};
 }
