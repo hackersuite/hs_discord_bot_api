58bf6456b1c3f17cbd4ed7781b3db9fe11fbb105
Amish Shah <amishshah.2k@gmail.com>
2020-04-23 20:31:55 +0100

    fix: typo

--- src/templates/messages.ts
+++ src/templates/messages.ts
@@ -5,11 +5,11 @@ export function teamWelcome(team: APITeam): CreateMessageData {
 	return {
 		embed: {
 			title: `Welcome "${team.name}"! ✨`,
-			description: 'This is your private team chat, only members of your team and organisers can access it.' +
+			description: 'This is your private team chat, only members of your team and organisers can access it. ' +
 			'The same goes for your voice channel!\n\n' +
 			'If members of your team are missing, make sure that they have signed up on the ' +
 			'[StudentHack website](https://auth.studenthack2020.com/) and have then joined the Discord server. ' +
-			'If they joined the server and _then_ joined the team, they may need to click the join button again.\n\n' +
+			'If they joined the server and _then_ joined the team, they may need to click the "Join Discord" button again.\n\n' +
 			'If you need any further assistance, feel free to message in the Hackathon channels above.\n\n' +
 			'Have fun and good luck!',
 			color: 0xfa66c1
